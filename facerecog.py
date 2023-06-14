143dababf7619a854bb086ac0239f2ec0e124053
xtLUCIFER <umr0042@gmail.com>
2023-06-14 12:12:25 +0500

    well organized code ,before uploading to github

--- facerecog.py
+++ facerecog.py
@@ -1,30 +1,47 @@
-import cv2 
+import cv2
 
-print("Program created by mr Umair")
+# Print program information
+print("Program created by Mr. Umair")
+
+# Load face cascade classifier
 face_cap = cv2.CascadeClassifier('/home/programmer/.local/share/virtualenvs/python-0P1cz62Y/lib/python3.10/site-packages/cv2/data/haarcascade_frontalface_default.xml')
+
+# Initialize video capture
 video_capt = cv2.VideoCapture(0)
 
-print("face is showing in camera & detector is enabled!")
-while True:
-    ret , video_data = video_capt.read()
-    col = cv2.cvtColor(video_data,cv2.COLOR_BGR2GRAY)
-    faces = face_cap.detectMultiScale(
-        col,
-        scaleFactor = 1.1,
-        minNeighbors = 5,
-        minSize = (30, 30),
-        flags = cv2.CASCADE_SCALE_IMAGE
+# Print information about face detection
+print("Face is being detected in the camera feed. The detector is enabled!")
 
-    )
-    for (x,y,w,h) in faces :
-        cv2.rectangle(video_data,(x,y),(x+w,y+h),(0,255,0),2)
-    cv2.imshow('video_live',video_data)
+while True:
+    # Read a frame from the video capture
+    ret, video_data = video_capt.read()
     
+    # Convert frame to grayscale
+    gray = cv2.cvtColor(video_data, cv2.COLOR_BGR2GRAY)
+    
+    # Detect faces in the grayscale frame
+    faces = face_cap.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+    
+    # Draw rectangles around the detected faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(video_data, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    
+    # Display the frame with face detections
+    cv2.imshow('Video Live', video_data)
+    
+    # Check for 'a' key press to exit the program
     if cv2.waitKey(20) == ord('a'):
         break
 
-
-
+# Release the video capture and close all windows
 video_capt.release()
 cv2.destroyAllWindows()
-print("program closed ...bye.")
+
+# Print program closing message
+print("Program closed. Goodbye!")
